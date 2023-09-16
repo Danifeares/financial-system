b74193386bf255df28eefd26259fa20cf8148080
Arianna Silveira <arianna.silveira.s@gmail.com>
2023-09-16 17:53:40 -0300

    refactor: limitação do campo de descricao de categorias realista

--- src/dump.sql
+++ src/dump.sql
@@ -3,26 +3,26 @@ CREATE DATABASE dindin;
 CREATE TABLE 
 	usuarios (
         id SERIAL PRIMARY KEY,
-        nome TEXT VARCHAR(255) NOT NULL,
-        email TEXT VARCHAR(255) NOT NULL UNIQUE,
-        senha TEXT VARCHAR(300) NOT NULL
+        nome VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        senha VARCHAR(300) NOT NULL
   );
 
 CREATE TABLE
 	categorias(
  		id SERIAL PRIMARY KEY,
-        descricao TEXT VARCHAR(500)
+        descricao VARCHAR(25)
  );
  
  CREATE TABLE
  	transacoes(
  		id SERIAL PRIMARY KEY,
- 		descricao TEXT VARCHAR(500),
+ 		descricao VARCHAR(500),
  		valor INTEGER NOT NULL,
  		data DATE NOT NULL,
  		categoria_id INTEGER REFERENCES categorias(id),
  		usuario_id INTEGER REFERENCES usuarios(id),
- 		tipo TEXT VARCHAR(100) NOT NULL
+ 		tipo VARCHAR(100) NOT NULL
  );
 
 INSERT INTO categorias (descricao) VALUES
