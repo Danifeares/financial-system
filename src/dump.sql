52e53573d911d1bc3672797205bd000655b81854
Arianna Silveira <arianna.silveira.s@gmail.com>
2023-09-16 17:52:11 -0300

    refactor: limitação de campos no script de banco

--- src/dump.sql
+++ src/dump.sql
@@ -3,26 +3,26 @@ CREATE DATABASE dindin;
 CREATE TABLE 
 	usuarios (
         id SERIAL PRIMARY KEY,
-        nome TEXT NOT NULL,
-        email TEXT NOT NULL UNIQUE,
-        senha TEXT NOT NULL
+        nome TEXT VARCHAR(255) NOT NULL,
+        email TEXT VARCHAR(255) NOT NULL UNIQUE,
+        senha TEXT VARCHAR(300) NOT NULL
   );
 
 CREATE TABLE
 	categorias(
  		id SERIAL PRIMARY KEY,
-        descricao TEXT
+        descricao TEXT VARCHAR(500)
  );
  
  CREATE TABLE
  	transacoes(
  		id SERIAL PRIMARY KEY,
- 		descricao TEXT,
+ 		descricao TEXT VARCHAR(500),
  		valor INTEGER NOT NULL,
  		data DATE NOT NULL,
  		categoria_id INTEGER REFERENCES categorias(id),
  		usuario_id INTEGER REFERENCES usuarios(id),
- 		tipo TEXT NOT NULL
+ 		tipo TEXT VARCHAR(100) NOT NULL
  );
 
 INSERT INTO categorias (descricao) VALUES
