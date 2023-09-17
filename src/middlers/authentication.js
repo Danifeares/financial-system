a6d0b871bbbece687cf86d3274a107ac1afd14d2
Arianna Silveira <arianna.silveira.s@gmail.com>
2023-09-17 03:01:04 -0300

    refactor: padronização inglês

--- src/middlers/authentication.js
+++ src/middlers/authentication.js
@@ -1,36 +1,42 @@
 const jwt = require('jsonwebtoken')
 const pool = require('../connection')
-const senhaJwt = require('../jwtPassword')
+const jwtPassword = require('../jwtPassword')
 
 const authentication = async (req, res, next) => {
     const { authorization } = req.headers
+    try {
 
-    if (!authorization) {
-        return res.status(401).json({message: 'Não autorizado!'})
-    }
+        if (!authorization) {
+            throw {
+                message: 'Para acessar este recurso um token de autenticação válido deve ser enviado.',
+                code: 401
+            }
+        }
 
-    const token = authorization.split(' ')[1]
+        const token = authorization.split(' ')[1]
 
-    try{
-        const { id } = jwt.verify(token, senhaJwt)
+        const { id } = jwt.verify(token, jwtPassword)
 
-		const { rows, rowCount } = await pool.query('SELECT * from usuarios WHERE id = $1', [id])
+        const { rows, rowCount } = await pool.query('SELECT * FROM usuarios WHERE id = $1', [id])
 
-		if (rowCount == 0) {
-			return res.status(401).json({ mensagem: 'Não autorizado' })
-		}
+        if (rowCount == 0) {
+            throw {
+                message: 'Para acessar este recurso um token de autenticação válido deve ser enviado.',
+                code: 401
+            }
+        }
 
-		const { senha, ...usuario } = rows[0]
+        delete rows[0].senha
 
-		req.usuario = usuario
+        req.user = rows[0]
 
-		next()
-
-    }
-    catch(error) {
+        next()
+        
+    } catch (error) {
         console.log(error.message)
-        return res.status(401).json({message: 'Não autorizado!'})
-    }
+        return res.status(error.code || 500).json({ message: error.message || 'Internal server error' })
+      }
+
 }
 
 module.exports = authentication
